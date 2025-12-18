0f81d21dd2a7df836dcd46bf441fec132d2d0dcc
md-shariff2026 <muhamedshariff2006@gmail.com>
2025-12-18 20:29:30 +0530

    somes

--- frontend/assets/js/ui.js
+++ frontend/assets/js/ui.js
@@ -1,13 +1,15 @@
 async function loadComponent(id, file) {
   const res = await fetch(file);
-  document.getElementById(id).innerHTML = await res.text();
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("header")) {
-    loadComponent("header", "/components/header.html");
+    loadComponent("header", "components/header.html");
   }
+
   if (document.getElementById("footer")) {
-    loadComponent("footer", "/components/footer.html");
+    loadComponent("footer", "components/footer.html");
   }
 });
